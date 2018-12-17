2888f0b358fdbef3fbf6b6369ed7281b33072a9a
Jefferson Agostinho <jefferson.andrade.agostinho@gmail.com>
2018-12-17 17:07:23 -0200

    Add cacheable

--- config/repository.php
+++ config/repository.php
@@ -4,17 +4,86 @@
 return [
 
     /*
-    |--------------------------------------------------------------------------
-    | Laravel Repository - Pagination Limit
-    |--------------------------------------------------------------------------
-    |
-    | A Laravel repository pattern package
-    |
-    | Configure the limit of items per page
-    */
+     |--------------------------------------------------------------------------
+     | Imovs Repository
+     |--------------------------------------------------------------------------
+     |
+     | A Laravel repository pattern
+     |
+     | Configure the repository package
+     */
 
     'pagination' => [
+
+        /*
+         |--------------------------------------------------------------------------
+         | Pagination limit
+         |--------------------------------------------------------------------------
+         |
+         | Set a default items per page
+         | Default: 15 items per page
+         */
+
         'limit' => 15
     ],
 
+    /*
+     |--------------------------------------------------------------------------
+     | Cache config
+     |--------------------------------------------------------------------------
+     |
+     | Configure cache
+     |
+     | Cache enabled: boolean
+     | Cache clean: boolean
+     | Cache time: integer
+     | Cache repository: Instance of Illuminate\Contracts\Cache\Repository
+     */
+
+    'cache' => [
+
+        /*
+         |--------------------------------------------------------------------------
+         | Cache enabled
+         |--------------------------------------------------------------------------
+         |
+         | Enable or disable cache
+         | Default: false
+         */
+
+        'enabled' => false,
+
+        /*
+         |--------------------------------------------------------------------------
+         | Cache clean
+         |--------------------------------------------------------------------------
+         |
+         | Enable or disable cache clean
+         | Default: true
+         */
+
+        'clean' => true,
+
+        /*
+         |--------------------------------------------------------------------------
+         | Cache time
+         |--------------------------------------------------------------------------
+         |
+         | Time of expiration cache in minutes
+         | Default: 30 minutes
+         */
+
+        'time' => 30,
+
+        /*
+         |--------------------------------------------------------------------------
+         | Cache repository
+         |--------------------------------------------------------------------------
+         |
+         | Instance of Illuminate\Contracts\Cache\Repository
+         | Default: Illuminate\Support\Facades\Cache::class
+         */
+
+        'repository' => Illuminate\Support\Facades\Cache::class,
+    ]
 ];
---
+++ src/Cache/Contracts/Cacheable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Imovs\Repository\Cache\Contracts;
+
+use Illuminate\Contracts\Cache\Repository as CacheRepository;
+
+/**
+ * Interface Cacheable
+ *
+ * @package \Imovs\Repository\Cache\Contracts
+ * @author Jefferson Agostinho <jefferson.andrade.agostinho@gmail.com>
+ */
+interface Cacheable
+{
+    /**
+     * Set Cache Repository
+     *
+     * @param \Illuminate\Contracts\Cache\Repository $cacheRepository
+     *
+     * @return $this
+     */
+    public function setCacheRepository(CacheRepository $cacheRepository);
+
+    /**
+     * Return instance of Cache Repository
+     *
+     * @return \Illuminate\Contracts\Cache\Repository
+     */
+    public function getCacheRepository();
+
+    /**
+     * Get Cache key for the method
+     *
+     * @param $method
+     * @param $args
+     *
+     * @return string
+     */
+    public function getCacheKey($method, $args = null);
+}
